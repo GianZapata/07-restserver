ea6f030b73d0d404f49df3fc0346bae72efc8b0e
GianZapata <elgianmc@hotmail.com>
2022-03-26 18:03:26 -0600

    Heroku

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 
 const app = express()
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
    res.send('Hello World12dwda3! types11232132123')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
    console.log('Example app listening on port 3000!3')
 })
