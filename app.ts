d60bf2f36a720d653f468f64c2f6b2905147e47a
GianZapata <elgianmc@hotmail.com>
2022-03-26 18:23:53 -0600

    Heroku

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
-import express from 'express'
+import express from 'express'; 
+import dotenv from 'dotenv'; 
 
-const app = express()
-const port = process.env.PORT || 5000;
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-   res.send('Hello World12dwda3! types11232132123')
-})
+	res.send('Hello World! 1232312313');
+});
 
 app.listen(port, () => {
-   console.log('Example app listening on port 3000!3')
-})
+	console.log('Example app listening on port', port);
+});
